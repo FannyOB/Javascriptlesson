7639357434ba00d21f07fffc87af4ea3f12df380
Fanny <fanny.olengabinene@laposte.net>
2024-06-11 22:57:10 +0200

    Cours Boucles

---
+++ Cours/Boucles/ExercicesBoucles.js
@@ -0,0 +1,13 @@
+// 1-Utilise une boucle for pour afficher les nombres pairs de 1 à 10.
+
+for(let i = 1;  i <= 10 ; i++){
+    //pour faire un test on utilise une condition
+    if(i % 2 === 0 ){ // si le nbre est divisible par 2, on regarde le reste de la division euclidienne
+        console.log(i)
+    }/*
+    else{
+        console.log(i, ": n'est pas un nombre pair")
+    }*/
+};
+
+// 2-
---
+++ Cours/Boucles/boucles.js
@@ -0,0 +1,62 @@
+// 1-Boucles FOR
+//Lorsque l'on SAIT A L'AVANCE COMBIEN DE FOIS le bloc doit être éxecuté
+//rappel let i = 0 signifie tu boucles à partir de la valeur 0
+
+for (let i = 0; i < 5; i++){
+    console.log("Numéro :", i);
+}
+
+
+// 2-Boucles WHILE
+//elle execute un bloc de code tant que la condition est vraie. 
+//Utile lorsque le nombre de répétitions N'EST PAS CONNU AVANT le début de la boucle.
+// exemple script lors d'un mot de passe à entrer (login)
+
+/*
+let motDePasse ="";
+
+while(motDePasse !== "monjoliemdp"){
+    let password = prompt("Entrez votre mot de passe!");
+    console.log("Tentive de connexion...")
+}
+console.log("Vous etes connecté.");
+*/
+
+// 3-Boucles DO...WHILE
+// est une variante de la boucle while qui GARANTIT QUE LE BLOC DE CODE EST EXECUTE AU MOINS UNE FOIS,
+// car la condition N'EST EVALUE QU'APRES LA 1ère EXECUTION .
+// DONC execution du code, puis vérification de la condition.
+
+let compteur = 0;
+
+do{
+    console.log("compteur", compteur);
+    compteur++;
+}while(compteur < 0);
+// compteur: 0
+
+
+// 4-Boucles FOR...OF pour PARCOURIR les TABLEAUX
+// FOR (avec "let" on défini 1 variable avec l'élément au singulier.) OF (Donc La variable de ce que l'on va parcourir=le tableau)
+
+let couleurs = ["rouge", "vert", "bleu"];
+
+for(let couleur of couleurs){
+    console.log("la couleur est: ", couleur);
+};
+
+
+// 5-Boucles FOR IN pour les OBJETS 
+// (avec "let" on défini 2 varibles, la variable de ce que l'on va parcourir , dans l'objet)
+
+let voiture = {
+    marque: "Toyota", //cle: valeur
+    modele: "Corolla",
+    annee: "2008"
+};
+
+for(let cle in voiture){
+    console.log(`clé ${cle}: ${voiture[cle]}`); //ES6 interpollation (utilisation des backticks)
+};
+
+
